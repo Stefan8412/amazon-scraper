0963b18fabe8311b97ac8ced52452aa4539e26ea
Stefan Hancar <stefan@hancar.sk>
2021-11-08 14:28:41 +0100

    fix bug

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const request = require("request-promise");
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 
 app.use(express.json());
-
-const generateScraperUrl= (apiKey)=>`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+const generateScraperUrl = (apiKey)=>`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 
 app.get("/",async (req,res)=>{
@@ -52,13 +52,14 @@ res.json(error)
 //get search result
 app.get("/search/:searchQuery",async (req,res)=>{
     const {searchQuery}=req.params;
+    const {api_key}=req.query;
     try{
-const response= await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+const response= await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
 res.json(JSON.parse(response));
     }catch(error){
 res.json(error)
     }
-})
+});
 
 app.listen(PORT, () =>
     console.log(`server running on port ${PORT}`)
