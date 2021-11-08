3ad6c1cbbc4fed494bede6fbab2a40311b12305f
Stefan Hancar <stefan@hancar.sk>
2021-11-08 14:00:25 +0100

    fix bug

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-
-
-const generateScraperUrl= (apiKey)=>`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+const generateScraperUrl= (apiKey)=>`http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
+
+app.get("/",async (req,res)=>{
     res.send("Welcome to Amazon Scraper API.");
 });
 
